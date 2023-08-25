5d8bacbc0bb99ce6d3de48a5625b14b6fb5ad02a
Drewry Pope <drewrypope@gmail.com>
2023-08-25 00:55:29 -0500

    Create lib.sh

---
+++ lib.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+get_submodule_status() {
+  submodule_path="$1"
+  submodule_status=$(git submodule status "$submodule_path")
+
+  # Check the first character of the output to determine the status
+  case "${submodule_status:0:1}" in
+    '-')
+      return 0
+      ;;
+    ' ')
+      return 1
+      ;;
+    '+')
+      return 2
+      ;;
+    *)
+      return 3
+      ;;
+  esac
+}
+
+echo_submodule_status() {
+  submodule_path="$1"
+  status_code="$2"
+
+  case $status_code in
+    0)
+      echo "The submodule at $submodule_path is not initialized."
+      ;;
+    1)
+      echo "The submodule at $submodule_path is initialized but not updated."
+      ;;
+    2)
+      echo "The submodule at $submodule_path is initialized and has changes."
+      ;;
+    3)
+      echo "Unknown status for the submodule at $submodule_path."
+      ;;
+  esac
+}
+
+submodule_status() {
+  submodule_path="$1"
+  get_submodule_status "$submodule_path"
+  status_code=$?
+  echo_submodule_status "$submodule_path" $status_code
+  return $status_code
+}
