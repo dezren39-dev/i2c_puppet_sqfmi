a52e2ff55c5aecc44fc6d14df8c035009277b6b5
drewry pope <drewrypope@gmail.com>
2023-08-25 00:20:44 -0500

    rename to beepy

--- dbuild.sh
+++ dbuild.sh
@@ -4,5 +4,5 @@ docker run --rm -it \
  -v $(pwd)/3rdparty/pico-sdk:/pico-sdk \
  -v $(pwd):/project \
  djflix/rpi-pico-builder:latest \
- bash -c 'mkdir -p build && cd build && cmake -DPICO_BOARD=bbq20kbd_breakout .. && make clean && make'
+ bash -c 'mkdir -p build && cd build && cmake -DPICO_BOARD=beepy .. && make clean && make'
  
